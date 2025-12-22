d9b3157354078e0b7685124e5f4d15eea4fe1f10
Kamil Garcarek <gyrekk.business@gmail.com>
2025-12-22 17:46:48 +0100

    kk

---
+++ backend/task-manager-backend/src/main/java/com/gyrekk/taskmanager/controller/TaskController.java
@@ -0,0 +1,4 @@
+package com.gyrekk.taskmanager.controller;
+
+public class TaskController {
+}
---
+++ backend/task-manager-backend/src/main/java/com/gyrekk/taskmanager/repository/InMemoryTaskRepository.java
@@ -0,0 +1,4 @@
+package com.gyrekk.taskmanager.repository;
+
+public class InMemoryTaskRepository {
+}
